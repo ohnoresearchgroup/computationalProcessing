05f43407419fffa3258540c5db54ea2682b4896b
Paul Ohno <pohno@users.noreply.github.com>
2024-05-13 10:29:14 -0500

    updated to find wls in log file

--- md/extractLRem.py
+++ md/extractLRem.py
@@ -5,45 +5,52 @@ Spyder Editor
 
 """
 
-import re
 import os
 
 #function to go through the file and find the correct energy
 def readFile(filename):
     # Open the file in read mode
+    targetstring = " Excitation energies and oscillator strengths:"
+    linenumberlastexcitation = 0;
     with open(filename, 'r') as file:
-        # Read all lines from the file
-        lines = file.readlines()
+        for linenumber, line in enumerate(file,1):
+            if line.startswith(targetstring):
+                linenumberlastexcitation = linenumber
+                
+    
+    targetstring = " Excited State"
+    oscstrengths = []
+    wavelengths = []
+    with open(filename, 'r') as file:
+        for linenumber, line in enumerate(file,1):
+            if linenumber < linenumberlastexcitation:
+                continue
+            if line.startswith(targetstring):                
+                #print(line)
+                split_line = line.split()
+                oscstrengths.append(float(split_line[8][2:]))
+                wavelengths.append(float(split_line[6]))
 
-    # Loop through each line and check if it starts with " After PCM"
-    for line in lines:
-        if line.startswith(" After PCM"):
-            # Split the line on spaces
-            split_line = line.split()
-
-            # Filter out substrings that represent floats
-            floats = [x for x in split_line if re.match(r'^-?\d+\.?\d*$', x)]
-        
-            #find first float which is energy
-            energy = float(floats[0])
-
-            break
-        
-    return energy
+    maxindex = oscstrengths.index(max(oscstrengths))
+    print("Found excited state #"  + str(maxindex+1),
+          " with osc strength =",oscstrengths[maxindex],
+          "and wavelength =", wavelengths[maxindex], "nm")
+    
+    return wavelengths[maxindex]
 
 #create list of subdirectories in LRve directory
 parentdir = "LRem"
 directories = [os.path.join(parentdir, d) for d in os.listdir(parentdir) if os.path.isdir(os.path.join(parentdir, d))]
 
 #go through that list of directories and read each energy
-energies = []
+wls = []
 for d in directories:
     filenames = os.listdir(d)
     for filename in filenames:
         if filename[-4:] == ".log":
-            energy = readFile(filename)
-            energies.append(energy)
-            
-print(energies)
+            wl = readFile(filename)
+            wls.append(wl)
+
+print(wls)
 
 
