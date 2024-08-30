ce0857064a21cb10d5bb5bbd458896627dc6a337
Paul Ohno <pohno@users.noreply.github.com>
2024-08-30 17:10:37 -0500

    CIP and SSIP
    updated iondistances.py and added calculateIP to calculate CIP and SSIP

--- md/iondistances.py
+++ md/iondistances.py
@@ -4,12 +4,13 @@ import pandas as pd
 import numpy as np
 
 #names of files
-paramfile = "solv1.parm7"
-trajfile = "prod_gs_1m1.dcd"
+paramfile = "isolv.parm7"
+trajfile = "prodimagedi.dcd"
 
 
-#frames you are selecting (this is from 25 to 1200, every 25)
-selectedframes = np.arange(25,1225,25)
+#frames you are selecting, None if all frames
+selectedframes = None
+#selectedframes = np.arange(25,1225,25)
 
 masks = ["@1-42","@1","@10","@15","@22"]
 
@@ -17,8 +18,19 @@ for mask in masks:
     with open('closest.cpptraj','w') as file:
         file.write("parm " + paramfile + "\n")
         file.write("trajin " + trajfile + "\n")
-        file.write("solvent :Na+,Cl-\n")
-        file.write("closest 1 " + mask + " closestout closestion" + mask[1:] + "\n" )
+        file.write("solvent :Na+\n")
+        file.write("closest 1 " + mask + " closestout closestNa" + mask[1:] + "\n" )
+        file.write("go")
+    
+    #run cpptraj file, then delete it
+    subprocess.run(["cpptraj","closest.cpptraj"])
+    os.remove("closest.cpptraj")
+    
+    with open('closest.cpptraj','w') as file:
+        file.write("parm " + paramfile + "\n")
+        file.write("trajin " + trajfile + "\n")
+        file.write("solvent :Cl-\n")
+        file.write("closest 1 " + mask + " closestout closestCl" + mask[1:] + "\n" )
         file.write("go")
     
     #run cpptraj file, then delete it
@@ -27,13 +39,25 @@ for mask in masks:
 
 
 for mask in masks:
-    df = pd.read_csv("closestion" + mask[1:], sep='\\s+')
-    df = df.drop(columns =["CLOSEST_00001[FirstAtm]","CLOSEST_00001[Mol]"])
-    df = df.rename(columns={'#CLOSEST_00001[Frame]': 'frame', "CLOSEST_00001[Dist]": 'distance'})
-    os.remove("closestion" + mask[1:])
+    df_Na = pd.read_csv("closestNa" + mask[1:], sep='\\s+')
+    df_Na = df_Na.drop(columns =["CLOSEST_00001[FirstAtm]","CLOSEST_00001[Mol]"])
+    df_Na = df_Na.rename(columns={'#CLOSEST_00001[Frame]': 'frame', "CLOSEST_00001[Dist]": 'distance_Na'})
+    os.remove("closestNa" + mask[1:])
+    
+    df_Cl = pd.read_csv("closestCl" + mask[1:], sep='\\s+')
+    df_Cl = df_Cl.drop(columns =["CLOSEST_00001[FirstAtm]","CLOSEST_00001[Mol]"])
+    df_Cl = df_Cl.rename(columns={'#CLOSEST_00001[Frame]': 'frame', "CLOSEST_00001[Dist]": 'distance_Cl'})
+    os.remove("closestCl" + mask[1:])
+    
+    df = df_Na
+    df['distance_Cl'] = df_Cl['distance_Cl']
+    
     
     #only choose from selected frames
-    filtereddf = df[df['frame'].isin(selectedframes)]
+    if selectedframes is not None:
+        filtereddf = df[df['frame'].isin(selectedframes)]
+    else:
+        filtereddf = df
     
     #create allhbonds.csv
     filtereddf.to_csv("closestion" + mask[1:] + ".csv", sep= ',',index=False,float_format='%.8f')
